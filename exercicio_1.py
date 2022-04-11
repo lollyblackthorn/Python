5268e0186fcb1bbcb9530cb6cbfe45bf2f1ec2bd
Lorrany <79610401+lollyblackthorn@users.noreply.github.com>
2022-04-11 12:58:13 -0300

    Update exercicio_1.py
    Estudando um pouco mais de lógica consegui entender onde eu estava errando. Resolução concluída.

--- exercicio_1.py
+++ exercicio_1.py
@@ -46,7 +46,50 @@ elif Sexo == "Outro":
 else:
   print("Informação sexo inválida!")
 #----------------------------------------------------------------------------------------------------------------------------------
-#3. Ainda não encontrei uma resolução plausível.
+'''3. Vamos fazer um programa para verificar quem é o assassino de um crime.
+Para descobrir o assassino, a polícia faz um pequeno questionário com 5
+perguntas onde a resposta só pode ser sim ou não:
+a. Mora perto da vítima?
+b. Já trabalhou com a vítima?
+c. Telefonou para a vítima?
+d. Esteve no local do crime?
+e. Devia para a vítima?
+Cada resposta sim dá um ponto para o suspeito. A polícia considera que os
+suspeitos com 5 pontos são os assassinos, com 4 a 3 pontos são cúmplices e
+2 pontos são apenas suspeitos, necessitando outras investigações. Valores
+iguais ou abaixo de 1 são liberados'''
+
+interrogado = input("Informe o nome do interrogado: ")
+perguntas = 0
+
+a = input("Mora perto da vítima? ")
+if a == "Sim":
+  perguntas = perguntas + 1
+
+b = input("Já trabalhou com a vítima? ")
+if b == "Sim":
+  perguntas = perguntas + 1
+
+c = input("Telefonou para a vítima? ")
+if c == "Sim":
+  perguntas = perguntas + 1
+
+d = input("Esteve no local do crime? ")
+if d == "Sim":
+  perguntas = perguntas + 1
+
+e = input("Devia para a vítima? ")
+if e == "Sim":
+  perguntas = perguntas + 1
+  
+if perguntas >= 5:
+  print(interrogado,"você está preso, é o assasino!!!")
+elif perguntas == 3 or perguntas == 4:
+  print(interrogado,"Está detido, é cúmplice do ocorrido!!")
+elif perguntas == 2:
+  print(interrogado,"É suspeito, faça mais algumas investigações!!")
+else: #Valores iguais ou abaixo de 1 são liberados
+  print(interrogado,"Liberado!!!")
 
 
 #-----------------------------------------------------------------------------------------------------------------------------------
